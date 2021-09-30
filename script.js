ea7574a34063ebe1f3a045c2b90de53bc37a8707
koskos1986 <ekosopletkin@yandex.ru>
2021-09-30 21:14:36 +0300

    add wrap for two left functions

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 
 const upperCaseBtn = document.getElementById("upper-case");
 const lowerCaseBtn = document.getElementById("lower-case");
+const properCaseBtn = document.getElementById("proper-case");
+const sentenceCaseBtn = document.getElementById("sentence-case");
 const textArea = document.getElementById("textarea");
 
 upperCaseBtn.addEventListener("click", function (){
-  let newtext = textArea.value.toUpperCase();
-  textArea.value = newtext;
+  let newText = textArea.value.toUpperCase();
+  textArea.value = newText;
 });
 
 lowerCaseBtn.addEventListener("click", function (){
-  let newtext = textArea.value.toLowerCase();
-  textArea.value = newtext;
+  let newText = textArea.value.toLowerCase();
+  textArea.value = newText;
+});
+
+properCaseBtn.addEventListener("click", function() {
+
+});
+
+sentenceCaseBtn.addEventListener("click", function() {
+
 });
